ba948dae1da342e365e6503448d20b6dc4fee4f9
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-03-22 15:49:47 +0100

    Identity server 4 MVC UI and Custome test users

---
+++ src/Hades.OAuth/Quickstart/TestUsers.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Hades.OAuth
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users = new List<TestUser>
+        {
+            new TestUser{SubjectId = "2645BD94-3624-43FC-B21F-1209D730FC71", Username = "Haris", Password = "haris",
+                Claims = new List<Claim>
+                {
+                    new Claim ("given_Name", "Haris"),
+                    new Claim ( "familiy_name", "Neko")
+                }
+            },
+            new TestUser{SubjectId = "88421113", Username = "bob", Password = "bob",
+                Claims = new List<Claim>
+                {
+                    new Claim ( "3F41DC87-E8DE-42EE-AC8D-355E4D3E1A2D", "Lejla"),
+                    new Claim ( "familiy_name", "Neko")
+                }
+            }
+        };
+    }
+}
