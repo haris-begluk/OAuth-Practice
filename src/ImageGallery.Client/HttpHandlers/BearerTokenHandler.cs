63ec88f7943d2b8be7ef50feafa2e5f8cc8281a7
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-03-23 12:50:00 +0100

    Refresh access token with long-lived access

--- src/ImageGallery.Client/HttpHandlers/BearerTokenHandler.cs
+++ src/ImageGallery.Client/HttpHandlers/BearerTokenHandler.cs
@@ -1,8 +1,11 @@
 ﻿using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,20 +15,67 @@ namespace ImageGallery.Client.HttpHandlers
     public class BearerTokenHandler : DelegatingHandler
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public BearerTokenHandler(IHttpContextAccessor httpContextAccessor)
+        public BearerTokenHandler(IHttpContextAccessor httpContextAccessor, IHttpClientFactory httpClientFactory)
         {
             _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
         }
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancelationToken)
         {
-            var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
-            var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            var accessToken = await GetAccesTokenAync();
             if (!string.IsNullOrEmpty(accessToken))
             {
                 request.SetBearerToken(accessToken);
             }
             return await base.SendAsync(request, cancelationToken);
+        }
+        public async Task<string> GetAccesTokenAync()
+        {
+            var expiresAt = await _httpContextAccessor.HttpContext.GetTokenAsync("expires_at");
+            var expiresAtAsDateTimeOfsett = DateTimeOffset.Parse(expiresAt, CultureInfo.InvariantCulture);
+            if ((expiresAtAsDateTimeOfsett.AddSeconds(-60)).ToUniversalTime() > DateTime.UtcNow)
+            {
+                return await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            }
+            var idClient = _httpClientFactory.CreateClient("IDPClient");
+            var discoveryDocument = await idClient.GetDiscoveryDocumentAsync();
+            var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            var refreshResponse = await idClient.RequestRefreshTokenAsync(new RefreshTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = "imagegalleryclient",
+                ClientSecret = "secret",
+                RefreshToken = refreshToken
+
+            });
+            var updatedTokens = new List<AuthenticationToken>();
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.IdToken,
+                Value = refreshResponse.IdentityToken
+            });
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.AccessToken,
+                Value = refreshResponse.AccessToken
+            });
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.RefreshToken,
+                Value = refreshResponse.RefreshToken
+            });
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = "expires_at",
+                Value = (DateTime.UtcNow + TimeSpan.FromSeconds(refreshResponse.ExpiresIn)).ToString("o", CultureInfo.InvariantCulture)
+            });
+
+            var currentAuthenticationResult = await _httpContextAccessor.HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            currentAuthenticationResult.Properties.StoreTokens(updatedTokens);
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, currentAuthenticationResult.Principal, currentAuthenticationResult.Properties);
+            return refreshResponse.AccessToken;
         }
     }
 }
