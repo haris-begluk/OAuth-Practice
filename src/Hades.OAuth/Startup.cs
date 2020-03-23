8941d43dbbd98b9212070be59a41f8ca2cd98fa3
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-03-23 22:59:37 +0100

    Persisting configuration and operational data

--- src/Hades.OAuth/Startup.cs
+++ src/Hades.OAuth/Startup.cs
@@ -2,11 +2,16 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Hades.OAuth
@@ -14,6 +19,7 @@ namespace Hades.OAuth
     public class Startup
     {
         public IWebHostEnvironment Environment { get; }
+        public string ConnString { get; } = "Server=localhost;Database=HadesIDPDB;Trusted_Connection=True;";
 
         public Startup(IWebHostEnvironment environment)
         {
@@ -26,13 +32,26 @@ namespace Hades.OAuth
             services.AddControllersWithViews();
 
             var builder = services.AddIdentityServer()
-                .AddInMemoryIdentityResources(Config.Ids)
-                .AddInMemoryApiResources(Config.Apis)
-                .AddInMemoryClients(Config.Clients)
                 .AddTestUsers(TestUsers.Users);
-
-            // not recommended for production - you need to store your key material somewhere secure
             builder.AddSigningCredential(LoadCertificateFromStore());
+
+            var migrationsAssembly = typeof(Startup)
+               .GetTypeInfo().Assembly.GetName().Name;
+
+            builder.AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(ConnString,
+                    options => options.MigrationsAssembly(migrationsAssembly));
+            });
+
+            builder.AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(ConnString,
+                    options => options.MigrationsAssembly(migrationsAssembly));
+            });
+
         }
 
         public void Configure(IApplicationBuilder app)
@@ -41,7 +60,7 @@ namespace Hades.OAuth
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            InitializeDatabase(app);
             // uncomment if you want to add MVC
             app.UseStaticFiles();
             app.UseRouting();
@@ -69,6 +88,45 @@ namespace Hades.OAuth
                     throw new Exception("The specified certificate wasn't found.");
                 }
                 return certCollection[0];
+            }
+        }
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider
+                    .GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider
+                    .GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.Apis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
             }
         }
     }
